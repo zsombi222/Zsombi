586c12f6f41e58fc6282ef04fb1c89db6923117d
zsombi222 <61201470+zsombi222@users.noreply.github.com>
2020-02-18 19:20:14 +0100

    Add files via upload

---
+++ f2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+
+double f(double x) {
+	if (x > 7) {
+		return (x / 113.90);
+	}
+	else {
+		return (pow(x, 4) * 0.444 - pow(x, 3) * 0.889 + pow(x, 2) * 56.88 + 7 * x - 7);
+	}
+
+}
+
+char myid[] = "A'rvai Zsombor,1MI,2,L2 <arvaizsombor@gmail.com> VC2WTQ\n";
+char mytask[] = "Feladat = 2\n";
+
+int main(void) {
+
+	setbuf(stdout, NULL);        // Kikapcsolja a bufferelest
+	printf(myid);
+	printf(mytask);
+	printf("program\n");
+
+
+
+	double d;
+	while(std::cin >> d)
+		std::cout << std::fixed << std::setprecision(4) << f(d) << std::endl;
+	
+	return 0;
+}
