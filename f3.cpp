82a5f4f5b852686d88f090c5bcf63db131576330
zsombi222 <61201470+zsombi222@users.noreply.github.com>
2020-02-23 18:26:02 +0100

    Add files via upload

---
+++ f3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+char myid[] = "A'rvai Zsombor,1MI,2,L2 <arvaizsombor@gmail.com> VC2WTQ\n";
+char mytask[] = "Feladat = 4\n";
+
+int main(void) {
+
+    setbuf(stdout, NULL);        // Kikapcsolja a bufferelest
+    printf(myid);
+    printf(mytask);
+    //printf("Program\n"); ///megváltoztatva nagy P re ez tökre nem kell mert nem lekérdezek
+
+    for (int i = 0; i < 6; i++) {
+        std::cout << "$__asd0x" << i << "V6.abc.qqriqB" << std::endl;
+    }
+    for (int i = 0; i < 229; i++) {
+        std::cout << "#" << i << std::endl;
+    }
+
+    return 0;
+}
